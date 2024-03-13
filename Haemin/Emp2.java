9e016c9418b19595a36245a968987f2a4c9623e1
Hyehmin <qqqwe10000@likelion.org>
2024-03-13 17:12:27 +0900

    git Second week Stduy - Haemin

---
+++ Haemin/Emp2.java
@@ -0,0 +1,14 @@
+package kr.or.kosa;
+import lombok.*;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+public class Emp2 {
+	private int empno;
+	private String ename;
+	private int sal;
+	
+}
+
+
